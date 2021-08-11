d85ac8a66d5633b578c6afd7abc4ddc9f7a775e2
WithoutRegret <mcwithoutregret@gmail.com>
2021-08-10 19:46:14 -0700

    Update armor colors

--- src/main/java/net/purelic/commons/profile/preferences/ArmorColor.java
+++ src/main/java/net/purelic/commons/profile/preferences/ArmorColor.java
@@ -1,57 +1,69 @@
 package net.purelic.commons.profile.preferences;
 
-import net.md_5.bungee.api.ChatColor;
-import org.apache.commons.lang.WordUtils;
-
-import java.util.Arrays;
+import java.awt.*;
 import java.util.List;
-import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public enum ArmorColor {
 
-    // Free Colors
-    RED(ChatColor.RED, false),
-    YELLOW(ChatColor.YELLOW, false),
-    GREEN(ChatColor.GREEN, false),
-    BLUE(ChatColor.BLUE, false),
-    AQUA(ChatColor.AQUA, false),
-    GRAY(ChatColor.GRAY, false),
-
-    // Premium Colors
-    DARK_RED(ChatColor.DARK_RED, true),
-    GOLD(ChatColor.GOLD, true),
-    DARK_GREEN(ChatColor.DARK_GREEN, true),
-    DARK_BLUE(ChatColor.DARK_BLUE, true),
-    DARK_AQUA(ChatColor.DARK_AQUA, true),
-    PINK(ChatColor.LIGHT_PURPLE, true),
-    PURPLE(ChatColor.DARK_PURPLE, true),
-    WHITE(ChatColor.WHITE, true),
-    DARK_GRAY(ChatColor.DARK_GRAY, true),
-    BLACK(ChatColor.BLACK, true),
+    YELLOW("#FFFF55"),
+    RED("#FF5555"),
+    BLUE("#5555FF"),
+    GREEN("#55FF55"),
+    OG_PURPLE("#8427FD"),
+    GOLD("#FFAA00"),
+    BUHLOSSOM_PINK("#D67689", "#EDB3C2", "#E9E9E9", "#D1D1D1"),
+    DART_RED("#191919", "#631312", "#191919", "#631312"),
+    OAF_TEAL("#016B71", "#003537", "#05F72C", "#00D561"),
+    BLACK("#000000"),
+    WHITE("#FFFFFF"),
+    PASTEL_GREEN("#BAFFC9"),
+    PASTEL_BLUE("#BAE1FF"),
     ;
 
-    private final String name;
-    private final ChatColor color;
-    private final boolean premium;
+    private final long helmet;
+    private final long chestplate;
+    private final long leggings;
+    private final long boots;
 
-    ArmorColor(ChatColor color, boolean premium) {
-        this.name = WordUtils.capitalizeFully(this.name().replaceAll("_", " "));
-        this.color = color;
-        this.premium = premium;
+    ArmorColor(String color) {
+        this(color, color);
     }
 
-    public String getName() {
-        return this.name;
+    ArmorColor(String top, String bottom) {
+        this(top, top, bottom, bottom);
     }
 
-    public ChatColor getColor() {
-        return this.color;
+    ArmorColor(String helmet, String chestplate, String leggings, String boots) {
+        this.helmet = this.getRGB(helmet);
+        this.chestplate = this.getRGB(chestplate);
+        this.leggings = this.getRGB(leggings);
+        this.boots = this.getRGB(boots);
     }
 
-    public boolean isPremium() {
-        return this.premium;
+    private int getRGB(String hex) {
+        Color color = Color.decode(hex);
+        int r = color.getRed();
+        int g = color.getGreen();
+        int b = color.getBlue();
+        return r << 16 | g << 8 | b;
+    }
+
+    public long getHelmet() {
+        return this.helmet;
+    }
+
+    public long getChestplate() {
+        return this.chestplate;
+    }
+
+    public long getLeggings() {
+        return this.leggings;
+    }
+
+    public long getBoots() {
+        return this.boots;
     }
 
     public static List<String> getNames() {
@@ -60,13 +72,6 @@ public enum ArmorColor {
 
     public static boolean contains(String value) {
         return ArmorColor.getNames().contains(value.toUpperCase());
-    }
-
-    public static ArmorColor random(boolean includePremium) {
-        List<ArmorColor> colors = Arrays.stream(ArmorColor.values())
-            .filter(color -> includePremium || !color.isPremium())
-            .collect(Collectors.toList());
-        return colors.get(new Random().nextInt(colors.size()));
     }
 
 }
