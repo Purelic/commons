64cba4d4411263d4d84a0775e93254b0f43cc7b7
WithoutRegret <mcwithoutregret@gmail.com>
2021-08-11 21:52:48 -0700

    Print skull owner name on right click

--- src/main/java/net/purelic/commons/listeners/PlayerInteract.java
+++ src/main/java/net/purelic/commons/listeners/PlayerInteract.java
@@ -4,9 +4,13 @@ import net.purelic.commons.Commons;
 import net.purelic.commons.utils.CommandUtils;
 import net.purelic.commons.utils.ItemCrafter;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.Skull;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -71,6 +75,21 @@ public class PlayerInteract implements Listener {
                 if (!opOnly || op) Bukkit.dispatchCommand(player, command);
             }
         }
+    }
+
+    @EventHandler
+    public void onPlayerInteractHead(PlayerInteractEvent event) {
+        if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            Player player = event.getPlayer();
+            BlockState block = event.getClickedBlock().getState();
+
+            if (block instanceof Skull) {
+                Skull skull = (Skull) block;
+                String owner = skull.getOwner();
+                CommandUtils.sendAlertMessage(player, "That's the head of " + ChatColor.DARK_AQUA + owner);
+            }
+        }
+
     }
 
 }
