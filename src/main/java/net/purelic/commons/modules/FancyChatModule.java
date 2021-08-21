1f9ce9b404d4be96b3b13af0f7a18def6291ba3f
WithoutRegret <mcwithoutregret@gmail.com>
2021-08-20 17:54:35 -0700

    Update fancy chat module to support chat channels

--- src/main/java/net/purelic/commons/modules/FancyChatModule.java
+++ src/main/java/net/purelic/commons/modules/FancyChatModule.java
@@ -1,5 +1,7 @@
 package net.purelic.commons.modules;
 
+import net.purelic.commons.Commons;
+import net.purelic.commons.profile.preferences.ChatChannel;
 import net.purelic.commons.utils.ChatUtils;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
@@ -8,8 +10,12 @@ public class FancyChatModule implements Module {
 
     @EventHandler
     public void onPlayerChat(AsyncPlayerChatEvent event) {
-        event.setCancelled(true);
-        ChatUtils.sendFancyChatMessage(event.getPlayer(), event.getMessage());
+        ChatChannel channel = Commons.getProfile(event.getPlayer()).getChatChannel();
+
+        if (channel == ChatChannel.GLOBAL || channel == ChatChannel.ALL) {
+            event.setCancelled(true);
+            ChatUtils.sendFancyChatMessage(event.getPlayer(), event.getMessage());
+        }
     }
 
 }
